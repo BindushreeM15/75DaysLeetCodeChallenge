8986ab00773aa8aaf79af565824b337ba57b5aff
Bindushree <maheshbindushree@gmail.com>
2026-03-12 19:40:56 +0530

    Time: 0 ms (100%), Space: 20.6 MB (40.13%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}  
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15], 9))  
